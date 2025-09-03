e94ea80a8e2bb40adad9f2914b4bd7446af53449
Hossein Azizdokht <h.azizdokht@gmail.com>
2025-09-03 07:54:31 +0330

    change db adress

--- db.js
+++ db.js
@@ -5,7 +5,10 @@ import mongoose from "mongoose";
 
 const connection = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/portfolio");
+    // await mongoose.connect("mongodb://127.0.0.1:27017/portfolio");
+    await mongoose.connect(
+      "mongodb+srv://admin:<db_password>@cluster0.tywnjio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/portfolio"
+    );
     console.log(chalk.green("Connected to MongoDB"));
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
