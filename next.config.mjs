0a886357c13b638b83aeac1492be68757dad7258
Hossein Azizdokht <87221042+Hossein-azizdokht@users.noreply.github.com>
2025-10-04 15:33:32 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,10 +9,7 @@ const { i18n } = require('./next-i18next.config')
 
 const nextConfig = {
   reactStrictMode: true,
-    i18n: {
-    defaultLocale: "fa",
-    locales: ["fa", "en"],
-  },
+    i18n,
 
 };
 
