5dfc03d4ee3668c5d61dd83c7f6e2d27149eb1e6
Hossein Azizdokht <87221042+Hossein-azizdokht@users.noreply.github.com>
2025-10-04 15:47:10 +0330

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,12 @@
+// next-i18next.config.js
 const path = require('path');
 
 module.exports = {
-  i18n: { 
-    locales: ["en", "fa"],
-    defaultLocale: "en",
-    localePath: path.resolve('./public/locales'),
+  i18n: {
+    defaultLocale: 'fa',
+    locales: ['fa', 'en'],
   },
+  localePath: typeof window === 'undefined' 
+    ? path.resolve('./public/locales') 
+    : '/public/locales', // مرورگر فقط مسیر نسبی میفهمه
 };
