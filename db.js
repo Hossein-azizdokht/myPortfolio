e6b763f239f1abfd2dc22a5702bb457f7e1211d6
Hossein Azizdokht <87221042+Hossein-azizdokht@users.noreply.github.com>
2025-10-04 14:43:37 +0330

    Update db.js

--- db.js
+++ db.js
@@ -6,9 +6,7 @@ import mongoose from "mongoose";
 const connection = async () => {
   try {
     // await mongoose.connect("mongodb://127.0.0.1:27017/portfolio");
-    await mongoose.connect(
-      "mongodb+srv://admin:qMcmBmVt1qbfrMQk@cluster0.tywnjio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/portfoilio"
-    );
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log(chalk.green("Connected to MongoDB"));
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
